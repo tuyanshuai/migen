8137607bb8ff8dd57671ddea62ef93cfceda800d
yanshuai <tuyanshuai@gmail.com>
2018-05-15 16:37:05 -0700

    change to class

--- tutorial/tutorial_sub.py
+++ tutorial/tutorial_sub.py
@@ -2,7 +2,7 @@
 """
 Spyder Editor
 
-This is a temporary script file.
+substractor
 """
 
 from migen import *
@@ -10,23 +10,16 @@ from migen.fhdl import verilog
 
 
 class Sub(Module):
-    def __init__(self, x, y, sub, borrow):
+    def __init__(self, width):
       
-         
-        self.sync += sub.eq(x - y)      
+        self.a = a = Signal(width)
+        self.b = b = Signal(width)
+        self.sub = sub = Signal(width) 
         
-        
-        self.sync += If(x>=y,
-                        borrow.eq(0)
-                        ).Else(
-                        borrow.eq(1) )
-
-
-Width = 10
-x = Signal(Width)
-y = Signal(Width)
-sub = Signal(Width)
-borrow = Signal()
-
+        self.borrow = borrow = Signal()
+        self.sync += sub.eq(a - b)
+        self.sync += If(a>=b, borrow.eq(0)).Else(borrow.eq(1))        
+      
 if __name__ == "__main__":
-    print(verilog.convert(Sub(x,y, sub,borrow), ios={x,y,sub,borrow}))
+    dut = Sub(10)
+    print(verilog.convert(dut, ios ={ dut.a, dut.b, dut.sub, dut.borrow}))
